b4b92ac8e24b9b141ad7dd4e0677eeddf82b3733
Erivaldo José da Silva Santos Júnior <166261560+ErivaldoJunior02@users.noreply.github.com>
2024-04-06 21:54:24 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+const botaoMostrarProjetos = document.querySelector('.btn-mostrar-projetos')
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
+
+
+botaoMostrarProjetos.addEventListener('click', () => {
+    projetosInativos.forEach(projetoInativo => {
+        projetoInativo.classList.add('ativo');
+    });
+
+    botaoMostrarProjetos.classList.add('remover');
+});
